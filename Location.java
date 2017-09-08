325b04a897a5e253359a31fb79c78bd897ae3096
strangest-quark <sowmiyanagarajan98@gmail.com>
2017-09-08 14:57:49 +0530

    Locations within radius

---
+++ Location.java
@@ -0,0 +1,17 @@
+/* package whatever */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class loc
+{
+	double x;
+	double y;
+
+	public loc(double a, double b)
+	{
+		this.x=a;
+		this.y=b;
+	} 
+}
