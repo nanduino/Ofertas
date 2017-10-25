fa4ddff1b9fac1147e93ef1b49265331e6d764f8
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2017-10-25 15:44:36 +0530

    List Locations within radius

---
+++ location/Location.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Location
+{
+	double x;
+	double y;
+
+	public Location(double a, double b)
+	{
+		this.x=a;
+		this.y=b;
+	} 
+}
